ae68f61cb25c7db9bfbc8812306a322f2de23eaa
gih <giovannanotiktok@gmail.com>
2024-08-28 08:32:06 -0300

    first commit

---
+++ revisao/ex1.py
@@ -0,0 +1,11 @@
+# o problema do chocolate
+'''vish'''
+import random
+while True:
+    numerO_aleatorio= random.randit(1,25)
+    resposta = input = ('deseja sortear outro numero') .strip() .lower()
+    if resposta != 's':
+    ('encerrando o sorteio')
+    break
+    
+    
